ac21fcb9717e1ae76db3ff3d22dfd9760406d0c5
alelozada <ale.lozadag@gmail.com>
2022-05-02 23:55:51 -0500

    https URL

--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const getWeather = async (latitude, longitude) => {
 
-  const URL = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
   const request = await axios.get(URL)
 
   return request
